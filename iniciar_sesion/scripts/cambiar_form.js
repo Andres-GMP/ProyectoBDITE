8e314b88765e07badd593f42a5af4fb6ddc54b2a
AxelTEC <95446278+AxelTEC@users.noreply.github.com>
2022-04-02 12:35:22 -0700

    Nuevo posible inicio de sesion

---
+++ iniciar_sesion/scripts/cambiar_form.js
@@ -0,0 +1,21 @@
+const d = document
+export function alternarFormulario(formLogin,formRegister){
+    const $formLogin = d.getElementById(formLogin)
+    const $formRegister = d.getElementById(formRegister)
+    $formRegister.style.display="none"
+    function cambiar(){
+        if($formLogin.style.display == "none"){
+            $formLogin.style.display = "flex"
+            $formRegister.style.display = "none"
+        }
+        else{
+            $formLogin.style.display = "none"
+            $formRegister.style.display = "flex"
+        }
+    }
+    d.addEventListener("click",(e)=>{
+        e.preventDefault()
+        if(e.target.matches("#login")||e.target.matches("#register")) cambiar()
+    
+    })
+} 
---
+++ iniciar_sesion/scripts/cambiar_recuperar.js
@@ -0,0 +1,15 @@
+const d = document;
+export function alternarRecuperar(frame,salir,olvido){
+    const $frameRecuperar = d.querySelector(frame)
+    $frameRecuperar.style.display = "none"
+    function cambiarRecuperar(){
+            $frameRecuperar.style.display == "none"? 
+            $frameRecuperar.style.display = "flex":
+            $frameRecuperar.style.display = "none"
+        }
+        d.addEventListener("click",(e)=>{
+            e.preventDefault();
+            if(e.target.matches(salir)||e.target.matches(olvido)) cambiarRecuperar();
+        
+        })
+} 
