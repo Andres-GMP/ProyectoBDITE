c67c80fd52f0c25d93ee045d70d8bdd23c9e2383
AxelTEC <95446278+AxelTEC@users.noreply.github.com>
2022-04-16 01:02:31 -0700

    x

---
+++ scripts/cambiar_fondo.js
@@ -0,0 +1,15 @@
+const d = document;
+export function alternarFondo(frame){
+    const $frame = d.querySelector(frame)
+    let imagenes = [
+        "images/img1.png",
+        "images/img2.jpg",
+        "images/img3.jpg"
+    ];
+    let i = 1;
+    setInterval(() => {
+        $frame.style.backgroundImage = `linear-gradient(rgba(16, 0, 26, 0.5), rgba(16, 0, 26, 0.5)), url(${imagenes[i]})`;
+        if (!(i++ < imagenes.length - 1)) i = 0;
+    }, 5000)
+    
+}
