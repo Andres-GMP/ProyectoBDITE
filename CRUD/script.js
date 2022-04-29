67c5bc328360aadf780df7984876b0b12f62b7b9
AxelTEC <95446278+AxelTEC@users.noreply.github.com>
2022-04-29 13:08:56 -0700

    frame consultar

--- CRUD/script.js
+++ CRUD/script.js
@@ -1,11 +1,9 @@
-const $info = document.querySelector(".info-registro");
+/* const $info = document.querySelector(".info-registro");
 const $contentReg = document.querySelector(".registro-container");
 const $pin = document.querySelector(".fixed");
 const $pin2 = document.querySelector(".btnPin img");
 let control = false;
-/* 
-    CODIGO HECHO AL AVENTON
-*/
+
 
 document.addEventListener("click", e =>{
     e.preventDefault();
@@ -38,4 +36,71 @@ document.addEventListener("click", e =>{
         
         
     }
-}) 
+})  */
+const d = document;
+import {alteranarVentanasConsultar} from "./scripts/pin-consultar.js";
+const $filtrarConsulta = d.querySelector(".filtrar-container");
+const $informacionConsulta = d.querySelector(".informacion-container");
+const $frameContainer = d.querySelector(".consultas-container");
+const $tablaConsulta = d.querySelector(".tabla-container");
+const $tbody = d.querySelector("#tabla-contendio");
+
+
+d.addEventListener("DOMContentLoaded",(e) => {
+    alteranarVentanasConsultar($filtrarConsulta,$informacionConsulta,$frameContainer,"#pin-filtrar","#pin-informacion",".btnPinFiltrarMostrar",".btnPinInformacionMostrar");
+    cargarArticulos();
+    
+})
+
+const camposNombres = ["cantidad","descripcion","modelo","serie","valor","marca","adquisicion","fechaAdquisicion","area","resguardante","estatus"]
+/* CheckboxPrueba */
+const $checkboxesCampos = d.querySelector("#campos-container");
+/* inputs set */
+let activo = false;
+d.addEventListener("click",(e)=>{
+    console.log(e.target)
+    if(e.target.matches("#click-selectCampos")){
+        $checkboxesCampos.style.display = activo?"none":"block";
+        activo = activo?false:true;
+        console.log(activo)
+    }
+    if(e.target.matches("td")){
+        camposNombres.forEach(el=>{
+            d.querySelector(`[nameCampo = "${el}"]`).value = d.querySelector(`#${e.target.parentNode.id} > [campo = "${el}"]`).innerText
+
+        })
+    }
+})
+
+function cargarArticulos(){
+    async function fetchArticulos(){
+        const response = await fetch("./helpers/articulos.json");
+        if(!response.ok) {
+            const message = `Un error ha ocurrido ${response.status}`;
+            throw new Error(message);
+        }
+        const articulos = await response.json();
+        articulos.Articulos.forEach((el,i)=>{
+            $tbody.innerHTML += `
+            <tr id = "row-consulta-${i}" style="background-color:${(i%2===0)?"var(--gray)":"white"};">
+                <td campo = "numInventario">${el.NumInventario}</td>
+                <td campo = "cantidad">${el.Cantidad}</td>
+                <td campo = "descripcion">${el.Descripcion}</td>
+                <td campo = "modelo">${el.Modelo}</td>
+                <td campo = "serie">${el.Serie}</td>
+                <td campo = "valor">${el.Valor}</td>
+                <td campo = "marca">${el.Marca}</td>
+                <td campo = "adquisicion">${el.Adquisicion}</td>
+                <td campo = "fechaAdquisicion">${el.FechaDeAdquisicion}</td>
+                <td campo = "area">${el.Area}</th>
+                <td campo = "resguardante">${el.Resguardante}</td>
+                <td campo = "estatus">${el.Estatus}</td>
+            </tr>
+            `
+        }) 
+    }
+    fetchArticulos().catch(err=>
+        err.message
+    )
+}
+
